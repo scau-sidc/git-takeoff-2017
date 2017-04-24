2f2a4580886b89c7d123dfae796473a292ff69b9
bringtree <bringtree@qq.com>
2017-04-24 23:58:11 +0800

    去掉一些无关的字符串

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ request('https://weather.com/zh-CN/weather/today/l/CHXX0037:1:CH')
     var temp = $(".today_nowcard-temp").text();
     var weather = $(".today_nowcard-phrase").text();
     var getTime = $(".today_nowcard-timestamp").text();
-    console.log("地区:" + city);
+    console.log("地区:" + city.split(', ')[0]);
     console.log("温度:" + temp);
     console.log("天气:" + weather);
-    console.log("更新时间:" + getTime);
+    console.log("更新时间:" + getTime.match(/[0-9]+:[0-9]+/g));
   }).catch(function (e) {
   console.log(e)
 });
