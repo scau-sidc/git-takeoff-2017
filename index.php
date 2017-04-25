6fc069cb3f95ec2b36eabb7faea371b4d343834c
Rytia <admin@zzfly.net>
2017-04-25 11:42:45 +0800

    完成 shell 执行脚本

--- index.php
+++ index.php
@@ -3,5 +3,5 @@ $url = "https://api.weather.com/v2/turbo/vt1observation?apiKey=c1ea9f47f6a88b9ac
 $source = file_get_contents($url);
 $result = json_decode($source)->vt1observation;
 //print_r($result);
-printf("今天是%s，广州今日%s，体感温度%s℃，气压%s毫巴，相对湿度%s，能见度%s公里，%s%skm/s \n",date("m月d日"),$result->phrase,$result->feelsLike,$result->altimeter,$result->humidity,$result->visibility,$result->windDirCompass,$result->windSpeed)
+printf("今天是%s，广州天气%s，温度%s℃，体感温度%s℃，气压%s毫巴，相对湿度%s，能见度%s公里，%s%skm/s 紫外线指数%s/10，强度%s，祝您愉拥有快的一天！\n",date("m月d日"),$result->phrase,$result->temperature,$result->feelsLike,$result->altimeter,$result->humidity,$result->visibility,$result->windDirCompass,$result->windSpeed,$result->uvIndex,$result->uvDescription)
 ?>
