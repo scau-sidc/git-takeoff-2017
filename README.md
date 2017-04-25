70814979a39be407b13c34287f04a3c7ce30f6a2
Rytia <admin@zzfly.net>
2017-04-25 11:05:46 +0800

    完成 PHP 解析数据

---
+++ README.md
@@ -0,0 +1,7 @@
+### getWeather 简易天气信息获取
+- 数据源来自 weather.com
+- 采用 PHP 编写
+
+#### 环境需求
+- Linux
+- PHP >= 5.0
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+$url = "https://api.weather.com/v2/turbo/vt1observation?apiKey=c1ea9f47f6a88b9acb43aba7faf389d4&format=json&geocode=23.12%2C113.25&language=zh-CN&units=m";
+$source = file_get_contents($url);
+$result = json_decode($source)->vt1observation;
+//print_r($result);
+printf("今天是%s，广州今日%s，体感温度%s℃，气压%s毫巴，相对湿度%s，能见度%s公里，%s%skm/s \n",date("m月d日"),$result->phrase,$result->feelsLike,$result->altimeter,$result->humidity,$result->visibility,$result->windDirCompass,$result->windSpeed)
+?>
